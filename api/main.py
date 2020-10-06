49c948f3a626d7c5d742a8bfafc092eb35ba9768
Dima Doroshev <dima@doroshev.com>
2020-10-06 23:15:08 +0200

    Remove add_timing_middleware

--- api/main.py
+++ api/main.py
@@ -1,22 +1,16 @@
-import logging
 from typing import cast
 
 from fastapi import FastAPI
-from fastapi_utils.timing import add_timing_middleware
 import sentry_sdk
 from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 
 from .config import Config
 from .routes import add_routes
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 
 def create_app() -> FastAPI:
     config = Config()
     app = FastAPI()
-    add_timing_middleware(app, record=logger.info, prefix="app", exclude="untimed")
     add_routes(app)
 
     if config.SENTRY_DSN:
